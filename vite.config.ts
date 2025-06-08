a84c68707cd9830fe1b4445cb743f03bbf8c2c4e
Arman Tang <td19941113@gmail.com>
2025-06-08 23:06:18 +0800

    build: rolldown

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
-import { splitChunks } from './build/split-chunks.ts'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -32,7 +31,20 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: splitChunks(),
+        advancedChunks: {
+          groups: [
+            {
+              name: 'vendor-react',
+              test: /\/react@19|\/react-dom|\/react-router/,
+            },
+            { name: 'vendor-tiptap', test: /\/@tiptap|\/prosemirror/ },
+            {
+              name: 'vendor-sentry',
+              test: /\/@sentry/,
+            },
+            { name: 'vendor', test: /\/node_modules/ },
+          ],
+        },
       },
     },
     sourcemap: true,
